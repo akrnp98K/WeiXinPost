3dbbcf9dfb2641e17a3c66ef50ba66db0fe6879c
akrnp98K <85418647+akrnp98K@users.noreply.github.com>
2023-03-24 12:24:49 +0800

    Update config.py

--- config.py
+++ config.py
@@ -82,11 +82,11 @@ classes = \
 
 # 模板 1：每日提醒模板
 # 本周是开学的第: {{weeks.DATA}} 周
- 今天是: {{date.DATA}}
- 城市： {{city.DATA}}
- 天气： {{weather.DATA}}
- 最低气温: {{min_temperature.DATA}}
- 最高气温: {{max_temperature.DATA}}
+今天是: {{date.DATA}}
+城市： {{city.DATA}}
+天气： {{weather.DATA}}
+最低气温: {{min_temperature.DATA}}
+最高气温: {{max_temperature.DATA}}
 # 今天是破壳日的第: {{love_day.DATA}} 天
 # 距离开学还有: {{birthday.DATA}} 天
 # ----------------今日课程----------------

9256c6d8ebc239a6df087f41ea5af9d7e4757012
akrnp98K <85418647+akrnp98K@users.noreply.github.com>
2023-03-24 12:52:24 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,15 +7,15 @@ app_secret = "e0825d82594ccbf769453230837e80a1"
 template_id = "4_L5cT_YVQEMhYTpCvy3lqcraVWu1UxBTEYgc0PJ0us"
 # 接收公众号消息的微信号
 #这是我的
-user = ""
+user = "oQWUJ6G38ChBMO9_1PkmUVoktKa8"
 #这是宝宝的
 # user = ""
 
 # 信息配置
 # 所在省份
-province = ""
+province = "湖北"
 # 所在城市
-city = ""
+city = "武汉"
 # 生日，如果月份或者日期小于10，直接用对应的数字即可，例如1997-1-1
 birthday = "2003-8-8"
 # 在一起的日子，格式同上
